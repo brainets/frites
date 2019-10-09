99f0d2ff11d27d776242e368d14e8dfad04d5b75
EtienneCmb <e.combrisson@gmail.com>
2019-10-09 11:09:11 +0200

    project creation

---
+++ frites/__init__.py
@@ -0,0 +1,14 @@
+"""
+Frites
+======
+
+Framework of Information Theory for Electrophysiological data and Statistics
+"""
+import logging
+
+
+# Set 'info' as the default logging level
+logger = logging.getLogger('frites')
+io.set_log_level('info')
+
+__version__ = "0.0.0"
---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# License: 3-clause BSD
+import os
+from setuptools import setup, find_packages
+
+__version__ = "0.0.0"
+NAME = 'frites'
+AUTHOR = "BraiNets"
+MAINTAINER = "Etienne Combrisson"
+EMAIL = 'e.combrisson@gmail.com'
+KEYWORDS = "information-theory statistics"
+DESCRIPTION = ("Framework of Information Theory for Electrophysiological data "
+               "and Statistics")
+URL = 'https://github.com/brainets/frites'
+DOWNLOAD_URL = ("https://github.com/brainets/frites/archive/v" +
+                __version__ + ".tar.gz")
+# Data path :
+PACKAGE_DATA = {}
+
+
+def read(fname):
+    """Read README and LICENSE."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name=NAME,
+    version=__version__,
+    packages=find_packages(),
+    package_dir={'frites': 'frites'},
+    package_data=PACKAGE_DATA,
+    include_package_data=True,
+    description=DESCRIPTION,
+    long_description=read('README.rst'),
+    platforms='any',
+    setup_requires=['numpy'],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "mne",
+        "pandas",
+        "joblib",
+    ],
+    dependency_links=[],
+    author=AUTHOR,
+    maintainer=MAINTAINER,
+    author_email=EMAIL,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    license=read('LICENSE'),
+    keywords=KEYWORDS,
+    classifiers=["Development Status :: 3 - Alpha",
+                 'Intended Audience :: Science/Research',
+                 'Intended Audience :: Education',
+                 'Intended Audience :: Developers',
+                 'Topic :: Scientific/Engineering :: Visualization',
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6",
+                 "Programming Language :: Python :: 3.7"
+                 ])
