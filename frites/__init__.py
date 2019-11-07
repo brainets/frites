68a920f4df0b543e2eaf8e4ab21baab3b005edf9
EtienneCmb <e.combrisson@gmail.com>
2019-11-07 20:13:13 +0100

    Support getting and setting frites' config

--- frites/__init__.py
+++ frites/__init__.py
@@ -8,8 +8,37 @@ import logging
 
 from frites import io, core, stats, utils, workflow, simulations  # noqa
 
+__version__ = "0.1.1"
+
+# -----------------------------------------------------------------------------
 # Set 'info' as the default logging level
 logger = logging.getLogger('frites')
 io.set_log_level('info')
 
-__version__ = "0.1.1"
+# -----------------------------------------------------------------------------
+# get / set config
+
+
+def get_config():
+    """Get the global configuration of frites."""
+    from frites.config import CONFIG
+    return CONFIG
+
+
+def set_config(key, value, verbose=None):
+    """Change the global config of frites.
+
+    Parameters
+    ----------
+    key : string
+        Entry of the config
+    value : dict / list
+        The new value for the selected key. The type should be the same as the
+        default one
+    """
+    io.set_log_level(verbose)
+    assert isinstance(key, str)
+    CONFIG = get_config()  # noqa
+    assert key in CONFIG.keys(), f"The key {key} doesn't exist."
+    CONFIG[key] = value
+    logger.info(f"The key {key} has been updated")
