e422f0042496e5fab2189fbac43388ea63f6fe19
EtienneCmb <e.combrisson@gmail.com>
2019-11-21 19:09:50 +0100

    fix flake8

--- frites/utils/wrapper.py
+++ frites/utils/wrapper.py
@@ -9,9 +9,9 @@ if config.CONFIG['USE_NUMBA'] and is_numba_installed():
     import numba
     def jit(signature=None, nopython=True, nogil=True, fastmath=True,  # noqa
             cache=True, **kwargs):
-            return numba.jit(signature_or_function=signature, cache=cache,
-                             nogil=nogil, fastmath=fastmath, nopython=nopython,
-                             **kwargs)
+        return numba.jit(signature_or_function=signature, cache=cache,
+                         nogil=nogil, fastmath=fastmath, nopython=nopython,
+                         **kwargs)
 else:
     def jit(*args, **kwargs):  # noqa
         def _jit(func):
