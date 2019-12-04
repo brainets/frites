1ebc53efc1540788184bba20ff095f204286d5f2
EtienneCmb <e.combrisson@gmail.com>
2019-12-04 15:33:59 +0100

    rm transfer entropy from gcmi_nd and move it to it.py

--- frites/core/__init__.py
+++ frites/core/__init__.py
@@ -12,8 +12,9 @@ from .gcmi_1d import (ent_1d_g, mi_1d_gg, gcmi_1d_cc, mi_model_1d_gd,  # noqa
                       cmi_1d_ggg, gccmi_1d_ccc, gccmi_1d_ccd)
 from .gcmi_nd import (mi_nd_gg, mi_model_nd_gd, cmi_nd_ggg, gcmi_nd_cc,  # noqa
                       gcmi_model_nd_cd, gccmi_nd_ccnd, gccmi_model_nd_cdnd,
-                      gccmi_nd_ccc, transfer_entropy)
+                      gccmi_nd_ccc)
 from .mi_stats import (permute_mi_vector) # noqa
+from .it import (it_transfer_entropy, it_fit)  # noqa
 
 # -----------------------------------------------------------------------------
 # get the core functions to use for the mi estimation
--- frites/stats/__init__.py
+++ frites/stats/__init__.py
@@ -4,6 +4,7 @@ from .stats_ffx import (ffx_maxstat, ffx_fdr, ffx_bonferroni,  # noqa
                         ffx_cluster_bonferroni, ffx_cluster_tfce)
 from .stats_rfx import (rfx_cluster_ttest, rfx_cluster_ttest_tfce)  # noqa
 from .stats_cluster import temporal_clusters_permutation_test  # noqa
+from .stats_param import ttest_1samp  # noqa
 
 STAT_FUN = dict(  # noqa
     ffx=dict(
