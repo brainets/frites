51711a568e507d4792402638e13573329a056d3a
EtienneCmb <e.combrisson@gmail.com>
2019-12-12 11:18:59 +0100

    cluster MCP {'maxstat', 'fdr', 'bonferroni'} + tests

--- frites/stats/stats_cluster.py
+++ frites/stats/stats_cluster.py
@@ -1,10 +1,16 @@
 """Cluster related functions."""
+import logging
+
 import numpy as np
 
 from mne.stats.cluster_level import _find_clusters
+from mne.stats import fdr_correction, bonferroni_correction
+
+logger = logging.getLogger('frites')
 
 
-def temporal_clusters_permutation_test(mi, mi_p, th, tail=1, **kwargs):
+def temporal_clusters_permutation_test(mi, mi_p, th, tail=1, mcp='maxstat',
+                                       alpha=0.05, **kwargs):
     """Infer p-values using nonparametric test on temporal clusters.
 
     Parameters
@@ -18,6 +24,10 @@ def temporal_clusters_permutation_test(mi, mi_p, th, tail=1, **kwargs):
     tail : {-1, 0, 1}
         Type of comparison. Use -1 for the lower part of the distribution,
         1 for the higher part and 0 for both
+    mcp : {'maxstat', 'fdr', 'bonferroni'}
+        Method to use for correcting p-values for the multiple comparison
+        problem. By default, the maximum cluster-size across time and space is
+        used.
     kwargs : dict | {}
         Additional arguments are send to the
         :func:`mne.stats.cluster_level._find_clusters`
@@ -30,7 +40,11 @@ def temporal_clusters_permutation_test(mi, mi_p, th, tail=1, **kwargs):
     # get variables
     n_perm, n_roi, n_times = mi_p.shape
     assert tail in [-1, 0, 1]
+    assert mcp in ['maxstat', 'fdr', 'bonferroni']
     kwargs['tail'] = tail
+
+    logger.info(f"    Cluster detection (mcp={mcp}; alpha={alpha}; "
+                f"tail={tail})")
 
     # -------------------------------------------------------------------------
     # identify clusters for the true mi
@@ -46,18 +60,26 @@ def temporal_clusters_permutation_test(mi, mi_p, th, tail=1, **kwargs):
     # -------------------------------------------------------------------------
     # identify clusters for the permuted mi
     cl_p_mass = []
-    for p in range(n_perm):
+    for r in range(n_roi):
         _cl_p_null = []
-        for r in range(n_roi):
+        for p in range(n_perm):
             _, __cl_p_null = _find_clusters(mi_p[p, r, :], th, **kwargs)
             # if no cluster have been found, set a cluster mass of 0
             if not len(__cl_p_null): __cl_p_null = [0]  # noqa
-            _cl_p_null += [__cl_p_null]
-        # if this roi has no clusters, just ignore it
-        if not len(_cl_p_null): continue  # noqa
-        # take the maximum size (MCP)
-        cl_p_mass += [np.r_[tuple(_cl_p_null)].max()]
-    cl_p_mass = np.array(cl_p_mass).reshape(1, -1)
+            # Max / Min cluster size across time
+            if tail == 1:  # max cluster size across time
+                _cl_p_null += [np.r_[tuple(__cl_p_null)].max()]
+            elif tail == -1:  # min cluster size across time
+                _cl_p_null += [np.r_[tuple(__cl_p_null)].min()]
+            elif tail == 0:  # max of absolute cluster size across time
+                _cl_p_null += [np.abs(np.r_[tuple(__cl_p_null)]).max()]
+        cl_p_mass += [_cl_p_null]
+    # array conversion of shape (n_roi, n_perm)
+    cl_p_mass = np.asarray(cl_p_mass)
+
+    # for maximum statistics, repeat the max across ROI
+    if mcp == 'maxstat':
+        cl_p_mass = np.tile(cl_p_mass.max(axis=0, keepdims=True), (n_roi, 1))
 
     # -------------------------------------------------------------------------
     # infer p-values by comparing cluster sizes
@@ -65,11 +87,20 @@ def temporal_clusters_permutation_test(mi, mi_p, th, tail=1, **kwargs):
     for r, (cl_g, clm_g) in enumerate(zip(cl_true, cl_mass)):
         for cl, clm in zip(cl_g, clm_g):
             if tail == 1:
-                pv = (clm <= cl_p_mass).sum(1) / n_perm
+                pv = (clm <= cl_p_mass[[r], :]).sum(1) / n_perm
             elif tail == -1:
-                pv = (clm >= cl_p_mass).sum(1) / n_perm
+                pv = (clm >= cl_p_mass[[r], :]).sum(1) / n_perm
             elif tail == 0:
-                pv = (np.abs(clm) <= np.abs(cl_p_mass)).sum(1) / n_perm
+                pv = (np.abs(clm) <= np.abs(cl_p_mass[[r], :])).sum(1) / n_perm
             pvalues[r, cl] = max(1. / n_perm, pv)
+
+    # -------------------------------------------------------------------------
+    # MCP for FDR and Bonferroni
+
+    if mcp is 'fdr':
+        pvalues = fdr_correction(pvalues, alpha)[1]
+    if mcp is 'bonferroni':
+        pvalues = bonferroni_correction(pvalues, alpha)[1]
+    pvalues = np.clip(pvalues, 0., 1.)
 
     return pvalues
--- frites/stats/tests/test_stats_cluster.py
+++ frites/stats/tests/test_stats_cluster.py
@@ -21,21 +21,25 @@ class TestStatsClusters(object):  # noqa
 
     def test_temporal_clusters_permutation_test(self):
         """Test function temporal_clusters_permutation_test."""
-        n_pts = 100
-        sl_neg = slice(20, 40)
-        sl_pos = slice(60, 80)
+        n_pts, n_roi = 10, 2
+        sl_neg = slice(2, 4)
+        sl_pos = slice(6, 8)
         sl_both = [sl_neg, sl_pos]
         # generate the mutual information
-        mi = rnd.uniform(0, 1, (5, n_pts))
-        mi_p = rnd.uniform(0, 1, (100, 5, n_pts))
+        mi = rnd.uniform(0, 1, (n_roi, n_pts))
+        mi_p = rnd.uniform(0, 1, (1000, n_roi, n_pts))
         mi[:, sl_neg] -= 1000
         mi[:, sl_pos] += 1000
-        # tail = -1
-        pv_neg = temporal_clusters_permutation_test(mi, mi_p, th=-100, tail=-1)
-        self._is_signi(pv_neg, sl_neg)
-        # tail = 1
-        pv_pos = temporal_clusters_permutation_test(mi, mi_p, th=100, tail=1)
-        self._is_signi(pv_pos, sl_pos)
-        # tail = 0
-        pv_both = temporal_clusters_permutation_test(mi, mi_p, th=100, tail=0)
-        self._is_signi(pv_both, sl_both)
+        for mcp in ['maxstat', 'fdr', 'bonferroni']:
+            # tail = -1
+            pv_neg = temporal_clusters_permutation_test(
+                mi, mi_p, th=-100, tail=-1, mcp=mcp)
+            self._is_signi(pv_neg, sl_neg)
+            # tail = 1
+            pv_pos = temporal_clusters_permutation_test(
+                mi, mi_p, th=100, tail=1, mcp=mcp)
+            self._is_signi(pv_pos, sl_pos)
+            # tail = 0
+            pv_both = temporal_clusters_permutation_test(
+                mi, mi_p, th=100, tail=0, mcp=mcp)
+            self._is_signi(pv_both, sl_both)
