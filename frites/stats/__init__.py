3b7c2d7efe7e2324a8d29d1c375bdf9d3083b820
EtienneCmb <e.combrisson@gmail.com>
2019-12-13 15:21:15 +0100

    parametric testing + cleaning unsed functions

--- frites/stats/__init__.py
+++ frites/stats/__init__.py
@@ -1,6 +1,5 @@
 """Fixed and random effect methods."""
-from .stats_rfx import (rfx_ttest)  # noqa
 from .stats_cluster import (temporal_clusters_permutation_test,  # noqa
                             cluster_threshold)
-from .stats_param import ttest_1samp  # noqa
+from .stats_param import (ttest_1samp, rfx_ttest)  # noqa
 from .stats_mcp import (permutation_mcp_correction)  # noqa
---
+++ frites/stats/tests/test_stats_param.py
@@ -0,0 +1,41 @@
+"""Test parametric functions."""
+import numpy as np
+
+from frites.stats.stats_param import (ttest_1samp, rfx_ttest)
+
+
+class TestParam(object):  # noqa
+
+    def test_ttest_1samp(self):
+        """Test function ttest_1samp."""
+        x = np.random.rand(2, 100, 3)
+        popmean = 0.
+        tt_sci = ttest_1samp(x, popmean, axis=1, method='scipy')
+        tt_mne = ttest_1samp(x, popmean, axis=1, method='mne')
+        assert tt_sci.shape == tt_mne.shape == (2, 3)
+
+    def test_rfx_ttest(self):
+        """Test function rfx_ttest."""
+        n_suj, n_roi, n_times, n_perm = 4, 5, 10, 50
+        x = [np.random.rand(n_suj, n_times) for _ in range(n_roi)]
+        x_p = [np.random.rand(n_perm, n_suj, n_times) for _ in range(n_roi)]
+        # basic version
+        tv, tv_p = rfx_ttest(x, x_p)
+        assert tv.shape == (n_roi, n_times)
+        assert tv_p.shape == (n_perm, n_roi, n_times)
+        # center
+        tv, tv_p = rfx_ttest(x, x_p, center=True)
+        assert tv.shape == (n_roi, n_times)
+        assert tv_p.shape == (n_perm, n_roi, n_times)
+        # zscore
+        tv, tv_p = rfx_ttest(x, x_p, zscore=True)
+        assert tv.shape == (n_roi, n_times)
+        assert tv_p.shape == (n_perm, n_roi, n_times)
+        # center + zscore
+        tv, tv_p = rfx_ttest(x, x_p, zscore=True, center=True)
+        assert tv.shape == (n_roi, n_times)
+        assert tv_p.shape == (n_perm, n_roi, n_times)
+        # t-tested
+        tv, tv_p = rfx_ttest(x, x_p, ttested=True)
+        assert tv.shape == (n_roi * n_suj, n_times)
+        assert tv_p.shape == (n_perm, n_roi * n_suj, n_times)
