14ac36c73a53237a4b7c462c210d888801d1f637
EtienneCmb <e.combrisson@gmail.com>
2020-08-06 14:39:43 +0000

    Make install

---
+++ _downloads/066dcc2e9d4187aca1d10c945ae4d41b/plot_ar_instantaneous.py
@@ -0,0 +1,78 @@
+"""
+AR : simulate common driving input
+==================================
+
+This example illustrates an autoregressive model that simulates a common
+driving input (i.e X->Y and X->Z) and how it is measured using the covariance
+based Granger Causality
+"""
+import numpy as np
+
+from frites.simulations import StimSpecAR
+from frites.conn import conn_covgc
+
+import matplotlib.pyplot as plt
+
+
+###############################################################################
+# Simulate 3 nodes 40hz oscillations
+# ----------------------------------
+#
+# Here, we use the class :class:`frites.simulations.StimSpecAR` to simulate an
+# stimulus-specific autoregressive model made of three nodes (X, Y and Z). This
+# network simulates a transfer X->Y and X->Z. X is then called a common driving
+# input for Y and Z
+
+
+ar_type = 'osc_40_3'  # 40hz oscillations
+n_stim = 3          # number of stimulus
+n_epochs = 50       # number of epochs per stimulus
+
+ss = StimSpecAR()
+ar = ss.fit(ar_type=ar_type, n_epochs=n_epochs, n_stim=n_stim)
+print(ar)
+
+###############################################################################
+# plot the network
+
+plt.figure(figsize=(5, 4))
+ss.plot_model()
+plt.show()
+
+###############################################################################
+# plot the data
+
+plt.figure(figsize=(7, 8))
+ss.plot(cmap='bwr')
+plt.tight_layout()
+plt.show()
+
+###############################################################################
+# plot the power spectrum density (PSD)
+
+plt.figure(figsize=(7, 8))
+ss.plot(cmap='Reds', psd=True)
+plt.tight_layout()
+plt.show()
+
+
+###############################################################################
+# Compute the Granger-Causality
+# -----------------------------
+#
+# We then compute and plot the Granger Causality. From the plot you can see
+# that there's indeed an information transfer from X->Y and X->Z and, in
+# addition, an instantaneous connectivity between Y.Z
+
+dt = 50
+lag = 5
+step = 2
+t0 = np.arange(lag, ar.shape[-1] - dt, step)
+gc = conn_covgc(ar, roi='roi', times='times', dt=dt, lag=lag, t0=t0,
+                n_jobs=-1)[0]
+
+# sphinx_gallery_thumbnail_number = 4
+plt.figure(figsize=(12, 10))
+ss.plot_covgc(gc)
+plt.tight_layout()
+plt.show()
---
+++ _downloads/43eb5818f1cfaacdc1714ddde0b11933/plot_ar_pairwise.py
@@ -0,0 +1,54 @@
+"""
+AR : pairwise illustration
+==========================
+
+This example illustrates a simple autoregressive model simulating a
+stimulus-specific information transfer from a source X to a target Y.
+"""
+from frites.simulations import StimSpecAR
+
+import matplotlib.pyplot as plt
+
+
+###############################################################################
+# Simulate 40hz oscillations
+# --------------------------
+#
+# Here, we use the class :class:`frites.simulations.StimSpecAR` to simulate an
+# stimulus-specific autoregressive model. For the pairwise models, you can
+# choose :
+# 
+#     * 'hga' : high-gamma burst
+#     * 'osc_40' / 'osc_20' : respectivelly 20hz and 40hz oscillations
+#     * 'ding_2' : pairwise Ding's model :cite:`ding2006granger`
+
+
+ar_type = 'osc_40'  # 40hz oscillations
+n_stim = 3          # number of stimulus
+n_epochs = 50       # number of epochs per stimulus
+
+ss = StimSpecAR()
+ar = ss.fit(ar_type=ar_type, n_epochs=n_epochs, n_stim=n_stim)
+
+###############################################################################
+# plot the network
+
+plt.figure(figsize=(5, 4))
+ss.plot_model()
+plt.show()
+
+###############################################################################
+# plot the data
+
+plt.figure(figsize=(7, 8))
+ss.plot(cmap='bwr')
+plt.tight_layout()
+plt.show()
+
+###############################################################################
+# plot the power spectrum density (PSD)
+
+plt.figure(figsize=(7, 8))
+ss.plot(cmap='Reds', psd=True)
+plt.tight_layout()
+plt.show()
