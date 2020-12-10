2f28bf04d40d3a416e6a2c5cdb9a4ddab202b28b
EtienneCmb <e.combrisson@gmail.com>
2020-12-10 21:18:12 +0100

    v0.3.7

--- frites/__init__.py
+++ frites/__init__.py
@@ -8,7 +8,7 @@ import logging
 
 from frites import io, core, conn, stats, utils, workflow, simulations  # noqa
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 
 # -----------------------------------------------------------------------------
 # Set 'info' as the default logging level
