4bb5d7c2c7c4fae7d08e6632927d16b28d4361bd
EtienneCmb <e.combrisson@gmail.com>
2021-02-04 14:44:11 +0100

    Smoke and functional tests for GCMIEstimator

--- frites/estimator/tests/test_gcmi_est.py
+++ frites/estimator/tests/test_gcmi_est.py
@@ -5,7 +5,7 @@ from frites.estimator import GCMIEstimator
 
 np.random.seed(0)
 
-n_times = 20
+n_times = 40
 n_mv = 2
 n_samples = 100
 x = np.random.rand(n_times, n_mv, n_samples)
@@ -14,11 +14,24 @@ y_d = np.array([0] * 50 + [1] * 50)
 z_c = np.random.normal(size=(n_times, n_mv, n_samples))
 z_d = np.random.randint(0, 2, (n_samples,))
 categories = np.array([0] * 40 + [1] * 30 + [0] * 30)
+# effect definition
+sl_effect = slice(15, 25)
+effect = np.zeros((n_times,))
+effect[sl_effect] = 1.
 
 
 class TestGCMIEstimator(object):
 
-    def test_mi_cc(self):
+    @staticmethod
+    def _compare_effects(effect, mi):
+        """Compare ground-truth effect with obtained MI."""
+        mi = mi.squeeze()
+        mi = (mi > (mi.min() + mi.max()) / 2).astype(float)
+        perc = 100 * (effect == mi).sum() / n_times
+        assert perc > 95
+
+    def test_smoke_cc(self):
+        """Smoke test for cc MI."""
         for cop in [True, False]:
             for cat in [None, categories]:
                 # tensor-based
@@ -29,7 +42,8 @@ class TestGCMIEstimator(object):
                 mi_v = c.estimate(x, y_c, categories=cat)
                 np.testing.assert_array_almost_equal(mi_t, mi_v)
 
-    def test_mi_cd(self):
+    def test_smoke_cd(self):
+        """Smoke test for cd MI."""
         for cop in [True, False]:
             for cat in [None, categories]:
                 # tensor-based
@@ -40,7 +54,8 @@ class TestGCMIEstimator(object):
                 mi_v = c.estimate(x, y_d, categories=cat)
                 np.testing.assert_array_almost_equal(mi_t, mi_v)
 
-    def test_mi_ccd(self):
+    def test_smoke_ccd(self):
+        """Smoke test for ccd MI."""
         for cop in [True, False]:
             for cat in [None, categories]:
                 # tensor-based
@@ -51,7 +66,8 @@ class TestGCMIEstimator(object):
                 mi_v = c.estimate(x, y_c, z=z_d, categories=cat)
                 np.testing.assert_array_almost_equal(mi_t, mi_v)
 
-    def test_mi_ccc(self):
+    def test_smoke_ccc(self):
+        """Smoke test for ccc MI."""
         for cop in [True, False]:
             for cat in [None, categories]:
                 # tensor-based
@@ -61,3 +77,66 @@ class TestGCMIEstimator(object):
                 c = GCMIEstimator(mi_type='ccc', tensor=False, copnorm=cop)
                 mi_v = c.estimate(x, y_c, z=z_c, categories=cat)
                 np.testing.assert_array_almost_equal(mi_t, mi_v)
+
+    def test_functional_cc(self):
+        """Functional test for cc MI."""
+        # build the testing data
+        x = np.random.rand(n_times, n_mv, n_samples)
+        y = np.random.normal(size=(1, 1, n_samples,))
+        x[sl_effect, :, :] += y
+        y = np.tile(y, (n_times, 1, 1))
+        # functional test
+        for cop in [True, False]:
+            for imp in [True, False]:
+                c = GCMIEstimator(mi_type='cc', tensor=imp, copnorm=cop)
+                mi = c.estimate(x, y)
+                self._compare_effects(effect, mi)
+
+    def test_functional_cd(self):
+        """Functional test for cd MI."""
+        # build the testing data
+        x = np.random.rand(n_times, n_mv, n_samples)
+        x[sl_effect, :, 0:50] += 10.
+        x[sl_effect, :, 50::] -= 10.
+        y = np.array([0] * 50 + [1] * 50)
+        # functional test
+        for cop in [True, False]:
+            for imp in [True, False]:
+                c = GCMIEstimator(mi_type='cd', tensor=imp, copnorm=cop)
+                mi = c.estimate(x, y)
+                self._compare_effects(effect, mi)
+
+    def test_functional_ccd(self):
+        """Functional test for ccd MI."""
+        # build the testing data
+        x = np.random.rand(n_times, n_mv, n_samples)
+        y_pos = np.random.normal(size=(1, 1, 50))
+        y_neg = np.random.normal(size=(1, 1, 50))
+        x[sl_effect, :, 0:50] += y_pos
+        x[sl_effect, :, 50::] -= y_neg
+        y = np.tile(np.concatenate((y_pos, y_neg), axis=2), (n_times, 1, 1))
+        z = np.array([0] * 50 + [1] * 50)
+        # functional test
+        for cop in [True, False]:
+            for imp in [True, False]:
+                c = GCMIEstimator(mi_type='ccd', tensor=imp, copnorm=cop)
+                mi = c.estimate(x, y, z=z)
+                self._compare_effects(effect, mi)
+
+    def test_functional_ccc(self):
+        """Functional test for ccc MI."""
+        # build the testing data
+        x = np.random.rand(n_times, n_mv, n_samples)
+        y = np.random.normal(size=(1, 1, n_samples,))
+        x[sl_effect, :, :] += y
+        y = np.tile(y, (n_times, 1, 1))
+        z = np.random.rand(n_times, n_mv, n_samples)
+        # functional test
+        for cop in [True, False]:
+            for imp in [True, False]:
+                c = GCMIEstimator(mi_type='ccc', tensor=imp, copnorm=cop)
+                mi = c.estimate(x, y, z=z)
+                self._compare_effects(effect, mi)
+
+if __name__ == '__main__':
+    TestGCMIEstimator().test_functional_ccc()
