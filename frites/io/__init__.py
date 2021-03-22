1b6ce60cd7736e192aa6f3831833d45dd37a3201
EtienneCmb <e.combrisson@gmail.com>
2021-03-22 16:11:14 +0100

    Remove testing dependancies for pandas / xarray (now fixed)

--- frites/io/__init__.py
+++ frites/io/__init__.py
@@ -1,5 +1,4 @@
 """I/O conversion functions."""
 from .io_attributes import Attributes  # noqa
-from .io_dependencies import (is_pandas_installed, is_xarray_installed,  # noqa
-                              is_numba_installed)
+from .io_dependencies import (is_numba_installed)  # noqa
 from .io_syslog import set_log_level, logger, verbose  # noqa
