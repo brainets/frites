e9414f7f08a676f48f0a828e26949072496edabd
EtienneCmb <e.combrisson@gmail.com>
2023-05-31 17:15:38 +0200

    Fix tests for python 3.10 and 3.11

--- setup.py
+++ setup.py
@@ -70,4 +70,5 @@ setup(
                  "Programming Language :: Python :: 3.8",
                  "Programming Language :: Python :: 3.9",
                  "Programming Language :: Python :: 3.10",
+                 "Programming Language :: Python :: 3.11",
                  ])
